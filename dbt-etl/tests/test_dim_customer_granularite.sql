664512728516a3627646d7e48ba8e35411ca47de
rosieluu <luu.bichhong@gmail.com>
2025-10-31 14:57:14 +0100

    ajout test uniqueness for dimension after snapshot add

--- dbt-etl/tests/test_dim_customer_granularite.sql
+++ dbt-etl/tests/test_dim_customer_granularite.sql
@@ -6,3 +6,18 @@ SELECT
 FROM {{ ref('dim_customer') }}
 GROUP BY customer_id
 HAVING COUNT(*) > 1
+
+
+-- Test d'unicité pour l'état actuel des snapshots
+-- Vérifie qu'il n'y a qu'un seul enregistrement "current" par clé
+
+-- Test pour snap_dim_customer
+SELECT 
+    customer_id,
+    COUNT(*) as nb_current_records
+FROM {{ ref('snap_dim_customer') }}
+WHERE dbt_valid_to IS NULL
+GROUP BY customer_id
+HAVING COUNT(*) > 1
+
+UNION ALL
--- dbt-etl/tests/test_dim_product_granularite.sql
+++ dbt-etl/tests/test_dim_product_granularite.sql
@@ -6,3 +6,13 @@ SELECT
 FROM {{ ref('dim_product') }}
 GROUP BY product_id
 HAVING COUNT(*) > 1
+
+
+-- Test pour snap_dim_product  
+SELECT 
+    product_id as customer_id,
+    COUNT(*) as nb_current_records
+FROM {{ ref('snap_dim_product') }}
+WHERE dbt_valid_to IS NULL
+GROUP BY product_id
+HAVING COUNT(*) > 1
